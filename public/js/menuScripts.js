a4a6e1ce28000d2391a1bd5d2d52a1f030487dce
unknown <jektchuz4@gmail.com>
2014-03-23 00:43:29 -0600

    Menu bar added, design, no functionality

---
+++ public/js/menuScripts.js
@@ -0,0 +1,18 @@
+(function(){
+
+	var mainMenu = $('#mainMenu'),
+		wWidth = $(window).width(),
+		wHeight = $(window).height();
+
+
+	//Function to have the necessary height
+	//for 5 squared buttons
+	mainMenu.css({
+		'height' : wWidth / 5,
+		'top' : wHeight -  wWidth / 5
+	});
+
+
+
+
+})();
